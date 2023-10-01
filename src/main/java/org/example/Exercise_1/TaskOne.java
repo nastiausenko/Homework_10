242d070294795aedc4360ffa4c87c45ea2036575
nastiausenko <Usenko.Anastasia@lll.kpi.ua>
2023-10-01 18:56:41 +0300

    optimize regex for phone numbers

--- src/main/java/org/example/Exercise_1/TaskOne.java
+++ src/main/java/org/example/Exercise_1/TaskOne.java
@@ -3,7 +3,10 @@ package org.example.Exercise_1;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+//import java.lang.reflect.Parameter;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TaskOne {
     public static void main(String[] args) {
@@ -11,11 +14,12 @@ public class TaskOne {
 
         try(FileInputStream fis = new FileInputStream(file);
             Scanner scanner = new Scanner(fis)) {
-            String availableNumber = "\\d{3}-\\d{3}-\\d{4}|\\(\\d{3}\\)\\s\\d{3}-\\d{4}";
+            Pattern phonePattern = Pattern.compile("\\d{3}-\\d{3}-\\d{4}|\\(\\d{3}\\)\\s\\d{3}-\\d{4}");
 
             while (scanner.hasNext()){
                 String number = scanner.nextLine();
-                if (number.matches(availableNumber)) {
+                Matcher matcher = phonePattern.matcher(number);
+                if (matcher.matches()) {
                     System.out.println(number);
                 }
             }
