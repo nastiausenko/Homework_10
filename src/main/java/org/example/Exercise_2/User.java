325e23c5eff8a418e2b5e4ab85f918b79c0e0b19
nastiausenko <Usenko.Anastasia@lll.kpi.ua>
2023-10-02 19:42:39 +0300

    add class User

---
+++ src/main/java/org/example/Exercise_2/User.java
@@ -0,0 +1,11 @@
+package org.example.Exercise_2;
+
+public class User {
+    private String name;
+    private Integer age;
+
+    public User(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+}
