3cd2572b77b5586d5f9af6ec26d205a1b26fece2
nastiausenko <Usenko.Anastasia@lll.kpi.ua>
2023-10-03 19:44:55 +0300

    complete main functionality

--- src/main/java/org/example/Exercise_3/TaskThree.java
+++ src/main/java/org/example/Exercise_3/TaskThree.java
@@ -1,8 +1,36 @@
 package org.example.Exercise_3;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class TaskThree {
 
     public static void main(String[] args) {
+        File file = new File("textFiles/words.txt");
 
+        try(FileInputStream fis = new FileInputStream(file);
+            Scanner scanner = new Scanner(fis)){
+            Map<String, Integer> wordFrequency = new HashMap<>();
+            
+            while (scanner.hasNext()){
+                String line = scanner.nextLine();
+                String[] words = line.split("\\s+");
+
+                for (String word: words) {
+                    wordFrequency.put(word, wordFrequency.getOrDefault(word, 0) + 1);
+                }
+
+                for (Map.Entry<String, Integer> entry: wordFrequency.entrySet()) {
+                    System.out.println(entry.getKey() + " " + entry.getValue());
+                }
+            }
+
+        } catch (IOException ex){
+            ex.printStackTrace();
+        }
     }
 }
